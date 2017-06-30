b9b6a5c65c4ba57fa8e9be99f056230eb2933568
Ludovic Claude <ludovic.claude54@gmail.com>
2017-06-30 16:42:52 +0200

    Update Hugo

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ else
   DOCKER="sudo docker"
 fi
 
-$DOCKER run --rm -P -v $(pwd):/src -v $(pwd)/../development-guidelines.pages/:/output/ jojomi/hugo:0.19
+$DOCKER run --rm -P -v $(pwd):/src -v $(pwd)/../development-guidelines.pages/:/output/ jojomi/hugo:0.24.1
 
 sudo chown -R $USER:$USER ../development-guidelines.pages/
